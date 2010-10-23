b74fa03af4dd94d5efa0cec8ee502d2bc9bafffb
Marc Brinkmann <git@marcbrinkmann.de>
2010-10-23 13:14:24 +0200

    Finished first version.

--- podloader.py
+++ podloader.py
@@ -5,30 +5,46 @@ import optparse
 import daemon
 import dateutil.rrule
 import logging
+import sys
+import feedparser
+import urllib
+import urllib2
+import time
+import os
 
 log = logging.getLogger('podloader')
 debug = log.debug
 info = log.info
+warn = log.warn
+fatal = log.fatal
 
-p = optparse.OptionParser()
+p = optparse.OptionParser(usage = '%prog [options] TARGETDIR URL')
 p.add_option('-F','--foreground',help="Do not daemonize",action="store_true")
 p.add_option('-i','--interval',help="Wait this long before refreshing",default="1 hour")
 p.add_option('-d','--debug',help="enable debugging output",action="store_true")
+p.add_option('-l','--limit',help="only consider latest n entries",type="int",default=-1)
+p.add_option('-L','--logfile',help="logfile",default="podloader.log")
+p.add_option('-m','--umask',help="set umask at program start",type="string")
 
 (opts, args) = p.parse_args()
 
-logging.basicConfig(level = logging.INFO if not opts.debug else logging.DEBUG)
+if not len(args) == 2:
+	p.print_usage()
+	sys.exit(1)
+
+targetdir, url = args
+
+logging.basicConfig(level = logging.INFO if not opts.debug else logging.DEBUG,
+                    filename = opts.logfile)
 
 intervals = {
-	's': dateutil.rrule.SECONDLY,
-	'm': dateutil.rrule.MINUTELY,
-	'h': dateutil.rrule.HOURLY,
-	'd': dateutil.rrule.DAILY,
-	'w': dateutil.rrule.WEEKLY,
-	'o': dateutil.rrule.MONTHLY,
-	'y': dateutil.rrule.YEARLY
+	's': 1,
+	'm': 60,
+	'h': 60*60,
+	'd': 60*60*24,
+	'w': 60*60*24*7,
 }
-def parse_interval(s, default_step = dateutil.rrule.MINUTELY):
+def parse_interval(s, default_step = intervals['h']):
 	debug('parsing interval string %s', s)
 	parts = s.lower().strip().split()
 
@@ -38,27 +54,56 @@ def parse_interval(s, default_step = dateutil.rrule.MINUTELY):
 	step = int(parts[0])
 	step_unit = default_step
 
-	if len(parts) > 1:
-		p = parts[1]
+	if len(parts) > 1: step_unit = intervals[parts[1][0]]
 
-		if p.startswith('month'): p = 'o'
-		step_unit = intervals[p[0]]
-
-	rv = dateutil.rrule.rrule(step_unit, interval = step)
-	if opts.debug:
-		i = rv.__iter__()
-		debug('next steps in rrule: %s %s %s', i.next(), i.next(), i.next())
-	return rv
+	return step*step_unit
 
 def main(ival):
 	debug('started')
+	debug('ival: %d', ival)
+
+	while True:
+		try:
+			debug('downloading feed')
+			data = urllib2.urlopen(url).read()
+			debug('parsing feed')
+			d = feedparser.parse(data)
+			info('updated feed %s', d['feed']['title'])
+		except urllib2.URLError, e:
+			warn('error downloading feed: %s', e)
+
+		for e in reversed(d['entries'][:opts.limit]):
+			for encl in e.enclosures:
+				debug('handling %s: %s', e['title'], encl['url'])
+
+				# check if file exists
+				filename = os.path.basename(encl['url'])
+				filepath = os.path.join(targetdir, filename)
+				if os.path.exists(filepath):
+					debug('skipping, %s exists', filepath)
+				else:
+					info('%s: downloading %s to %s', e['title'], encl['url'], filepath)
+					urllib.urlretrieve(encl['url'], filepath)
+					info('finished downloading')
+
+		debug('done, sleeping')
+		time.sleep(ival)
 
 if '__main__' == __name__:
 	# parse options
 	ival = parse_interval(opts.interval)
-	if not opts.foreground:
-		with daemon.DaemonContext():
-			debug('daemonized')
+
+	if opts.umask:
+		umask = int(opts.umask, 8)
+		old_umask = os.umask(umask)
+		debug('umask now %o, from %o', umask, old_umask)
+	try:
+		if not opts.foreground:
+			with daemon.DaemonContext():
+				debug('daemonized')
+				main(ival)
+		else:
 			main(ival)
-	else:
-		main(ival)
+	except Exception, e:
+		fatal('Exception: %s', e)
+		raise
